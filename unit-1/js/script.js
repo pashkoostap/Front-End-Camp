a7c6d5ce497e66892fbf2d4746206e1aee1afe09
Pashko Ostap <pashko.ostap@gmail.com>
2017-02-17 15:21:45 +0200

    unit-1: chat messages

--- unit-1/js/script.js
+++ unit-1/js/script.js
@@ -1,4 +1,31 @@
 (function () {
+    window.addEventListener('load', function () {
+        setChatSize();
+    })
+    window.addEventListener('resize', function () {
+        setChatSize();
+    })
+    window.addEventListener('orientationchange', function () {
+        setChatSize();
+    })
+    // CALCULATE CHAT SIZE
+    function setChatSize() {
+        var windowHeight = window.innerHeight;
+        var rightChatHeaderHeight = document.querySelector('.right-chat-header').clientHeight;
+        var rightChatFormHeight = document.querySelector('.right-chat-form').clientHeight;
+        var chatHeight;
+        var rightChatMessagesWrapHeight;
+        if (windowHeight < 1000) {
+            chatHeight = 'height: ' + windowHeight + 'px;';
+            rightChatMessagesWrapHeight = 'height: ' + (windowHeight - rightChatHeaderHeight - rightChatFormHeight) + 'px;';
+        } else {
+            chatHeight = 'height: 700px;';
+            rightChatMessagesWrapHeight = 'height: 530px;';
+        }
+
+        document.querySelector('.osp-chat').setAttribute('style', chatHeight);
+        document.querySelector('.right-chat-messages').setAttribute('style', rightChatMessagesWrapHeight)
+    }
     // OPEN CHAT USERS
     var openUsersButton = document.querySelector('.left-chat-nav__open');
     var leftChatWrapper = document.querySelector('.left-chat-wrap');
@@ -13,6 +40,6 @@
             leftChatWrapper.classList.remove('left-chat-wrap--visible');
             leftChatWrapper.classList.add('left-chat-wrap--hidden');
         }
-        
     })
+
 })()
