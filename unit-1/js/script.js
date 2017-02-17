4f8af4d55ef5bc58384a412e6e88d07d7cbf839f
Pashko Ostap <pashko.ostap@gmail.com>
2017-02-17 20:15:08 +0200

    unit-1: login form functionality

--- unit-1/js/script.js
+++ unit-1/js/script.js
@@ -41,5 +41,52 @@
             leftChatWrapper.classList.add('left-chat-wrap--hidden');
         }
     })
+    // LOGIN TABS
+    var loginNavigation = document.querySelector('.osp-chat-form-nav');
+    var loginNavigationButtons = document.querySelectorAll('.osp-chat-form-nav__btn')
+    var loginFormWrap = document.querySelector('.osp-chat-form--login');
+    var signInFormWrap = document.querySelector('.osp-chat-form--sign-in');
+    function removeButtonsClasses(){
+        loginNavigationButtons.forEach(function(btn){
+            btn.classList.remove('osp-chat-form-nav__btn--active');
+        })
+    }
+    function changesFormsVisibility(formToShow, formToHide) {
+        formToShow.classList.remove('osp-chat-form--hidden');
+        formToHide.classList.add('osp-chat-form--hidden');
+    }
+    function addElementClass(elem, classToAdd) {
+        elem.classList.add(classToAdd);
+    }
+    loginNavigation.addEventListener('click',function(e){
+        var clickedButton = e.target;
+        if (clickedButton.className.indexOf('osp-chat-form-nav__btn--active') == -1) {
+            if (clickedButton.className.indexOf('osp-chat-form-nav__btn--login') != -1) {
+                removeButtonsClasses();
+                addElementClass(clickedButton, 'osp-chat-form-nav__btn--active');
+                changesFormsVisibility(loginFormWrap, signInFormWrap);
+                // clickedButton.classList.add('osp-chat-form-nav__btn--active');
+                // loginFormWrap.classList.remove('osp-chat-form--hidden');
+                // signInFormWrap.classList.add('osp-chat-form--hidden');
+            } else {
+                removeButtonsClasses();
+                addElementClass(clickedButton, 'osp-chat-form-nav__btn--active');
+                changesFormsVisibility(signInFormWrap, loginFormWrap)
+                // clickedButton.classList.add('osp-chat-form-nav__btn--active');
+                // loginFormWrap.classList.add('osp-chat-form--hidden');
+                // signInFormWrap.classList.remove('osp-chat-form--hidden');
+            }
+        }
+    })
+    // SHOW CHAT WRAPPER AFTER LOGIN SUBMIT
+    var loginSubmitBtn = document.querySelector('.osp-chat-form__submit--login');
+    var chatFormWrap = document.querySelector('.osp-chat-form-wrap');
+    var chatWrapper = document.querySelector('.osp-chat');
 
+    loginSubmitBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        addElementClass(chatFormWrap, 'osp-chat-form-wrap--hidden')
+        addElementClass(chatWrapper, 'osp-chat--visible')
+        setChatSize();
+    })
 })()
